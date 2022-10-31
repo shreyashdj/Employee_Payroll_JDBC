520621ff8a1f89d1349e6a4b6d8fa8e4ca5eb638
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-31 18:58:43 +0530

    [Shreyash]: Added code to update salary of Terissa to 3000000

--- EmployeePayrollJDBC/src/main/java/com/bridgelabz/employeePayrollJDBC/EmployeePayrollService.java
+++ EmployeePayrollJDBC/src/main/java/com/bridgelabz/employeePayrollJDBC/EmployeePayrollService.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.employeePayrollJDBC;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author - Shreyash Jadhav
@@ -11,24 +14,42 @@ public class EmployeePayrollService {
     /**
      *  Method to get all employee payroll data
      */
-    public void readEmployeePayrollData() throws SQLException {
-            PreparedStatement preparedStatement = DatabaseConnection.connection.prepareStatement("select * from employee_payroll");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                System.out.println("\n *********************************");
-                System.out.println("\n EmployeeID   : " + resultSet.getString("EmployeeID"));
-                System.out.println(" EmployeeName : " + resultSet.getString("EmployeeName"));
-                System.out.println(" Address      : " + resultSet.getString("Address"));
-                System.out.println(" PhoneNumber  : " + resultSet.getString("PhoneNumber"));
-                System.out.println(" Department   : " + resultSet.getString("Department"));
-                System.out.println(" Gender       : " + resultSet.getString("Gender"));
-                System.out.println(" StartDate    : " + resultSet.getString("StartDate"));
-                System.out.println(" BasicPay     : " + resultSet.getString("BasicPay"));
-                System.out.println(" Deductions   : " + resultSet.getString("Deductions"));
-                System.out.println(" TaxablePay   : " + resultSet.getString("TaxablePay"));
-                System.out.println(" IncomeTax    : " + resultSet.getString("IncomeTax"));
-                System.out.println(" NetPay       : " + resultSet.getString("NetPay"));
-            }
-        System.out.println("\n *********************************");
+    public int readEmployeePayrollData() throws SQLException {
+        List<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
+        String query = "select * from employee_payroll";
+        PreparedStatement preparedStatement = DatabaseConnection.connection.prepareStatement(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            int employeeId = resultSet.getInt("EmployeeID");
+            String name = resultSet.getString("EmployeeName");
+            int salary = resultSet.getInt("BasicPay");
+            Date startDate = resultSet.getDate("StartDate");
+            employeePayrollDataList.add(new EmployeePayrollData(employeeId, name, salary, startDate));
         }
+        System.out.println("\n ****** Data of All Employees from employee_payroll table ***** \n");
+        employeePayrollDataList.forEach(System.out::println);
+        return employeePayrollDataList.size();
+    }
+    public String readAnyColumnDataByEmployeeName(String empName, String columnName) throws SQLException {
+        String query = "select " + columnName + " from employee_payroll where EmployeeName = ?";
+        PreparedStatement preparedStatement = DatabaseConnection.connection.prepareStatement(query);
+        preparedStatement.setString(1, empName);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        String result = null;
+        if (resultSet.next()) {
+            result = resultSet.getString(columnName);
+        }
+        return result;
+    }
+    /**
+     *  Method to update base salary of employee
+     */
+    public void updateBaseSalary(String name, double salary) throws SQLException {
+        String query = "update employee_payroll set BasicPay = ? where EmployeeName = ?";
+        PreparedStatement preparedStatement = DatabaseConnection.connection.prepareStatement(query);
+        preparedStatement.setDouble(1, salary);
+        preparedStatement.setString(2, name);
+        preparedStatement.executeUpdate();
+    }
 }
